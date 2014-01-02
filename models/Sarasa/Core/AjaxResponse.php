78c62b672f913f3afca928daa5e98350a0e9d396
Joaquín Miguez <joacodeviaje@gmail.com>
2014-01-02 13:18:02 -0300

    fixes and extendable

--- models/Sarasa/Core/AjaxResponse.php
+++ models/Sarasa/Core/AjaxResponse.php
@@ -2,55 +2,74 @@
 
 namespace Sarasa\Core;
 
-class AjaxResponse {
-	
-	private $_responses;
-	
-	function AjaxResponse() {
-		$this->_responses = array();
-	}
+class AjaxResponse
+{
 
-	public function assign($id, $value, $val = NULL) {
-		if ($val && $value == 'innerHTML') $value = $val;
-		$this->_responses[] = array('assign', $id, $value);
-	}
+    protected $_responses;
+    
+    public function __construct()
+    {
+        $this->_responses = array();
+    }
 
-	public function script($script) {
-		$this->_responses[] = array('script', $script);
-	}
+    public function assign($id, $value, $val = null)
+    {
+        if ($val && $value == 'innerHTML') {
+            $value = $val;
+        }
 
-	public function redirect($url) {
-		$this->_responses[] = array('redirect', $url);
-	}
+        $this->_responses[] = array('assign', $id, $value);
+    }
 
-	public function log($text) {
-		$this->_responses[] = array('log', $text);
-	}
+    public function script($script)
+    {
+        $this->_responses[] = array('script', $script);
+    }
 
-	public function fadeOut($id) {
-		$this->_responses[] = array('fadeout', $id);
-	}
+    public function redirect($url)
+    {
+        $this->_responses[] = array('redirect', $url);
+    }
 
-	public function append($id, $type, $html = NULL) {
-		if (!$html) $html = $type;
-		$this->_responses[] = array('append', $id, $html);
-	}
+    public function log($text)
+    {
+        $this->_responses[] = array('log', $text);
+    }
 
-	public function prepend($id, $type, $html = NULL) {
-		if (!$html) $html = $type;
-		$this->_responses[] = array('prepend', $id, $html);
-	}
+    public function fadeOut($id)
+    {
+        $this->_responses[] = array('fadeout', $id);
+    }
 
-	public function alert($value) {
-		$this->script('alert("'.$value.'");');
-	}
+    public function append($id, $type, $html = null)
+    {
+        if (!$html) {
+            $html = $type;
+        }
 
-	public function remove($value) {
-		$this->script('$("#'.$value.'").remove();');
-	}
-	
-	public function toJSON() {
-		return json_encode($this->_responses);
-	}
-	
+        $this->_responses[] = array('append', $id, $html);
+    }
+
+    public function prepend($id, $type, $html = null)
+    {
+        if (!$html) {
+            $html = $type;
+        }
+        $this->_responses[] = array('prepend', $id, $html);
+    }
+
+    public function alert($value)
+    {
+        $this->script('alert("'.$value.'");');
+    }
+
+    public function remove($value)
+    {
+        $this->script('$("#'.$value.'").remove();');
+    }
+    
+    public function toJSON()
+    {
+        return json_encode($this->_responses);
+    }
 }
